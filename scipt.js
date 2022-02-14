8a9f8237fa7a91342fdcaa2a6785174b29d218b2
Ilona Drobot <drobotilonna@gmail.com>
2022-02-14 18:31:24 +0200

    homework9

---
+++ scipt.js
@@ -0,0 +1,23 @@
+
+let p = prompt ('Введите число');
+
+
+f:function sayhello(p) {
+    
+        if(p>0){
+            for(let i = 1;i<=p;i++){
+                console.log("hello", i)
+            }
+
+        }
+        else{
+            console.log("Error")
+        }
+    
+   
+    return sayhello
+}
+   sayhello(a=p)
+   
+
+   
